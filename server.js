4cf33dc704f6db2d566d5f43ed2a1695f8fd8126
YaelPrat <prat.yael@gmail.com>
2022-01-13 10:47:33 +0200

    Conect the Css file, first design of login page(need more work...)

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"));
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('./login.html'));
 });
