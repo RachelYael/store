402fe9b047d3e3195d2c2c17fb8e5258f89dd0fd
YaelPrat <prat.yael@gmail.com>
2022-01-24 15:53:57 +0200

    fix some responsive pages & try to put script in js fiels

---
+++ scripts/addProdHandler.js
@@ -0,0 +1,45 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCqlh0Inx6FfT4rr6KhZQSbw4VAWSmGozg",
+    authDomain: "web-cosmetic.firebaseapp.com",
+    projectId: "web-cosmetic",
+    storageBucket: "web-cosmetic.appspot.com",
+    messagingSenderId: "1064931964995",
+    appId: "1:1064931964995:web:f49c960b56eff8f9fcfcf6",
+    measurementId: "G-Q826CPQ94E"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+
+
+document.getElementById('add').onclick = function (req,res){
+    let title = document.getElementById("title");
+    let price = document.getElementById("price");
+    let stock = document.getElementById("stock");
+    if(title.value == "" || price.value == "" || stock.value == ""){
+        alert("You must fill all fields")
+        return;
+    }
+    if(isNaN(price.value) || isNaN(stock.value) || parseFloat(price.value) <= 0 || parseInt(stock.value) < 0){
+        alert("Price and Stock must be numbers: \nPrice bigger than 0\nStock at least 0")
+        return;
+    }
+    if(!Number.isInteger(Number(stock.value))){
+        alert("Stock must be of type integer")
+        return;
+    }
+    db.collection("products").doc(title.value).set({
+        Title: title.value,
+        Price: price.value,
+        Stock: stock.value
+    }).then(() => alert("Added product Successfully"),()=>  
+    document.getElementById("title").value='',
+    document.getElementById("title").value='', 
+    document.getElementById("price").value='',
+    document.getElementById("stock").value='' ).catch(e => alert(e.message));
+
+
+}
+
+
---
+++ scripts/usersHandler.js
@@ -0,0 +1,55 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCqlh0Inx6FfT4rr6KhZQSbw4VAWSmGozg",
+    authDomain: "web-cosmetic.firebaseapp.com",
+    projectId: "web-cosmetic",
+    storageBucket: "web-cosmetic.appspot.com",
+    messagingSenderId: "1064931964995",
+    appId: "1:1064931964995:web:f49c960b56eff8f9fcfcf6",
+    measurementId: "G-Q826CPQ94E"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+// auth.onAuthStateChanged(user=>{
+//     console.log(user)
+// })
+const db = firebase.firestore();
+
+var email, password;
+function ready(){
+    email = document.getElementById('email');
+    password = document.getElementById('password');
+    // auth.onAuthStateChanged(user=>{
+    //     alert(user)
+    // })
+}
+
+document.getElementById('login').onclick = function (req,res){
+    ready();
+    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+    promise.then(() => window.location.assign("./pages/main-menu.html"));
+    promise.catch(e => alert(e.message));
+}
+
+document.getElementById('register').onclick = function(){
+    ready();
+    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+    promise.then(() => alert("Registered Successfully"));
+    promise.catch(e => alert(e.message));
+}
+
+// if (window.performance && window.performance.navigation.type === window.performance.navigation.TYPE_BACK_FORWARD) {
+//     location.reload();
+// }
+
+const logout = document.querySelector('#logout');
+logout.addEventListener('click',(e)=>{
+    e.preventDefault();
+    auth.signOut().then(()=>{
+        // alert("log out");
+        // window.open('login.html');
+        // window.close('main-menu.html');
+        window.location.assign('login.html');
+    } ).catch((error) => {
+        alert(`Error ` +error)
+    });
+})
